06efee960ed172576ac30c1ad42418b7e8756dd7
Thahaseen <mthahaseen@gmail.com>
2016-01-31 22:50:29 +0530

    Added Swipe Refresh Layout

--- app/src/main/java/com/telstra/telstraapp/activity/MainActivity.java
+++ app/src/main/java/com/telstra/telstraapp/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.telstra.telstraapp.activity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -29,13 +30,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener{
 
     private ProgressDialog pDialog;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private static String LOG_TAG = "MainActivity";
+    private SwipeRefreshLayout swipeRefreshLayout;
     ConnectionDetector cd = new ConnectionDetector(this);
 
     @Override
@@ -43,17 +45,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pDialog = new ProgressDialog(MainActivity.this);
-
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-
+        swipeRefreshLayout.setOnRefreshListener(this);
         //Check for data connection before making JSON Request
         if(cd.isConnectingToInternet()){
             pDialog.setMessage(AppConstants.MSG_LOADING);
             pDialog.show();
             pDialog.setCancelable(false);
+            getItemData();
+        }else{
+            Toast.makeText(MainActivity.this, AppConstants.MSG_NO_CONNECTION, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        if(cd.isConnectingToInternet()) {
+            swipeRefreshLayout.setRefreshing(true);
             getItemData();
         }else{
             Toast.makeText(MainActivity.this, AppConstants.MSG_NO_CONNECTION, Toast.LENGTH_LONG).show();
@@ -84,10 +96,12 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, AppConstants.TOAST_DATA_ERROR, Toast.LENGTH_LONG).show();
                         }
                         pDialog.dismiss();
+                        swipeRefreshLayout.setRefreshing(false);
                     } catch (JSONException e) {
                         e.printStackTrace();}
                 }else {
                     pDialog.dismiss();
+                    swipeRefreshLayout.setRefreshing(false);
                     Toast.makeText(MainActivity.this, AppConstants.TOAST_DATA_ERROR, Toast.LENGTH_LONG).show();
                 }
             }
@@ -96,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 VolleyLog.d(LOG_TAG, "Error: " + error.getMessage());
                 pDialog.dismiss();
+                swipeRefreshLayout.setRefreshing(false);
                 Toast.makeText(MainActivity.this, AppConstants.TOAST_DATA_ERROR, Toast.LENGTH_LONG).show();
             }
         });
